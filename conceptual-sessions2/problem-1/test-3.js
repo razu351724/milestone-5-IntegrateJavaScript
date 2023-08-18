b413e42c045e56b35458ee5cc7650cd07629facb
RAZU-sheikh <razu351724@gmail.com>
2023-08-19 04:26:11 +0600

    assienment related website

--- conceptual-sessions2/problem-1/test-3.js
+++ conceptual-sessions2/problem-1/test-3.js
@@ -1,7 +1,7 @@
 function handleClick(data){
-    console.log(data.parentNode) // ছেলে বলা হচ্ছে তোমার পিতা কে
+    console.log(data.parentNode.childNodes) // ছেলে বলা হচ্ছে তোমার পিতা কে
     // console.log(data.parentNode.childNodes ) // পিতা কে বলা হচ্ছে তোমার ছেলেমেয়ে কয়জন 
-    console.log(data.parentNode.childNodes[2].innerText) // পিতা কে বলা হচ্ছে তোমার প্রথম ছেলেকে দাও, এবং কি পরা আছে দেখাও
+    console.log(data.parentNode.childNodes[3].innerText) // পিতা কে বলা হচ্ছে তোমার প্রথম ছেলেকে দাও, এবং কি পরা আছে দেখাও
 } 
 
 // const number = ['gias', 'hero', 'bd'];
