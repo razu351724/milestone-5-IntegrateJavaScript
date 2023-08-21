a628478d5cb0807e5c6ad981fe92a6c5b3259ad0
RAZU-sheikh <razu351724@gmail.com>
2023-08-21 19:15:14 +0600

    inbuton commplet in code update

--- AssignmentPractice-5/script.js
+++ AssignmentPractice-5/script.js
@@ -8,30 +8,43 @@ function mouchClickBtn(target){
     const price = target.childNodes[3].childNodes[5].childNodes[0].innerText
     total = parseFloat(total) + parseFloat(price);
     const databuby = document.getElementById('totalPrice').innerText = total;
-    
-var discountPercentage = 20
-
-var discountedPrice = (total * (discountPercentage / 100));
-const tofixtDiscountedPrice = discountedPrice.toFixed(2);
-document.getElementById('discount').innerText = tofixtDiscountedPrice;
-
-const tataolAmount = total - tofixtDiscountedPrice;
-
-document.getElementById('totalAmount').innerText = tataolAmount;
+    document.getElementById('totalAmount').innerText = databuby ;
 
 
-const purchaseButton = document.getElementById('makePurchase');
+    const purchaseButton = document.getElementById('makePurchase');
     if (total > 0) {
         purchaseButton.removeAttribute('disabled');
     } else {
         purchaseButton.setAttribute('disabled', true);
     }
 
-
-
-
+    const applybutton = document.getElementById('applybtn');  
+    if (total > 200) {
+        applybutton.removeAttribute('disabled');
+    } else {
+        applybutton.setAttribute('disabled', true);
+    }
 }
 
+
+document.getElementById('applybtn').addEventListener('click',function(){
+
+    const discountInput = document.getElementById('discountInput').value;
+    discountInput.value = '';
+    console.log(discountInput)
+    if(discountInput === 'SELL200'){
+
+    var discountPercentage = 20
+    var discountedPrice = (total * (discountPercentage / 100));
+    const tofixtDiscountedPrice = discountedPrice.toFixed(2);
+    document.getElementById('discount').innerText = tofixtDiscountedPrice;
+    const tataolAmount = total - tofixtDiscountedPrice;
+    document.getElementById('totalAmount').innerText =tataolAmount;
+    
+    }else{
+        alert('plice')
+    }
+})
 
 
 
